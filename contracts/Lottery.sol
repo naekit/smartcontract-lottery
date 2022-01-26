4c8bb28a0e452d0a0caaff94cc93c8aed66abf60
Mykyta <mykyta.onishchuck@code.berlin>
2022-01-26 20:43:58 +0100

    import vrf consumerbase and generate random number in endlottery

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
-contract Lottery is Ownable {
+contract Lottery is VRFConsumerBase, Ownable {
     address payable[] public players;
     uint256 public usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
@@ -14,15 +15,25 @@ contract Lottery is Ownable {
         CALCULATING_WINNER
     }
     LOTTERY_STATE public lottery_state;
+    uint256 public fee;
+    bytes32 public keyhash;
 
     // 0
     // 1
     // 2
 
-    constructor(address _priceFeedAddress) public {
+    constructor(
+        address _priceFeedAddress,
+        address _vrfCoordinator,
+        address _link,
+        uint256 _fee,
+        bytes32 _keyhash
+    ) public VRFConsumerBase(_vrfCoordinator, _link) {
         usdEntryFee = 50 * (10**18);
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
         lottery_state = LOTTERY_STATE.CLOSED; // we can put 1 here as well, this more readable
+        fee = _fee;
+        keyhash = _keyhash;
     }
 
     function enter() public payable {
@@ -52,15 +63,18 @@ contract Lottery is Ownable {
     }
 
     function endLottery() public {
-        uint256(
-            keccak256(
-                abi.encodePacked(
-                    nonce, // nonce predictable (transaction #)
-                    msg.sender, // msg.sender is predictable
-                    block.difficulty, // can actually be manipulated by the miners
-                    block.timestamp // predictable
-                )
-            )
-        ) % players.length;
+        // uint256(
+        //     keccak256(
+        //         abi.encodePacked(
+        //             nonce, // nonce predictable (transaction #)
+        //             msg.sender, // msg.sender is predictable
+        //             block.difficulty, // can actually be manipulated by the miners
+                    // block.timestamp // predictable 
+        //         )
+        //     )
+        // ) % players.length;
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+        bytes32 requestID = requestRandomness(keyhash, fee); // tqer
     }
-}
+
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness) internal override // vrf consumer base calls function, chainlink calls vrf consumerbase
