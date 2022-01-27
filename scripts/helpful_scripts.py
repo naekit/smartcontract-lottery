98ec33bdf6c782cf7cad678426545a1255023886
Mykyta <mykyta.onishchuck@code.berlin>
2022-01-27 17:32:10 +0100

    import vrfcoordinatormock and mockv3aggregator, compiled successfully

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,4 +1,11 @@
-from brownie import network, config, accounts, MockV3Aggregator, Contract
+from brownie import (
+    network,
+    config,
+    accounts,
+    MockV3Aggregator,
+    VRFCoordinatorMock,
+    Contract,
+)
 
 
 FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
@@ -21,7 +28,10 @@ def get_account(index=None, id=None):
     return accounts.add(config["wallets"]["from_key"])
 
 
-contract_to_mock = {"eth_usd_price_feed": MockV3Aggregator}
+contract_to_mock = {
+    "eth_usd_price_feed": MockV3Aggregator,
+    "vrf_coordinator": VRFCoordinatorMock,
+}
 
 
 def get_contract(contract_name):
