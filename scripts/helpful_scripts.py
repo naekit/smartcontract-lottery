16e7aeb6fabefb466ea572ccee91df6678d47eb0
Mykyta <mykyta.onishchuck@code.berlin>
2022-01-27 17:47:07 +0100

    get fee, key hash, and link token addresses and plug them for our constructor

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -32,7 +32,7 @@ def get_account(index=None, id=None):
 contract_to_mock = {
     "eth_usd_price_feed": MockV3Aggregator,
     "vrf_coordinator": VRFCoordinatorMock,
-    "link_tokne": LinkToken,
+    "link_token": LinkToken,
 }
 
 
