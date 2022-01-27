0e01caa000f60b8daac275306a56cc8d817b8af8
Mykyta <mykyta.onishchuck@code.berlin>
2022-01-27 20:49:12 +0100

    add unit tests, emit even w requestId in order to mock chain link node request, and test picking winner and transferring funds to his wallet with static random number

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -19,6 +19,7 @@ contract Lottery is VRFConsumerBase, Ownable {
     LOTTERY_STATE public lottery_state;
     uint256 public fee;
     bytes32 public keyhash;
+    event RequestedRandomness(bytes32 requestId);
 
     // 0
     // 1
@@ -76,7 +77,8 @@ contract Lottery is VRFConsumerBase, Ownable {
         //     )
         // ) % players.length;
         lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
-        bytes32 requestID = requestRandomness(keyhash, fee); // tqer
+        bytes32 requestId = requestRandomness(keyhash, fee); // tqer
+        emit RequestedRandomness(requestId);
     }
 
     function fulfillRandomness(bytes32 _requestId, uint256 _randomness)
---
+++ tests/test_lottery_unit.py
@@ -0,0 +1,96 @@
+from aiohttp import request
+import pytest
+from brownie import Lottery, accounts, config, network, exceptions
+from scripts.deploy_lottery import deploy_lottery
+from web3 import Web3
+import pytest
+
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    fund_with_link,
+    get_account,
+    get_contract,
+)
+
+
+def test_get_entrance_fee():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    # Arrange
+    lottery = deploy_lottery()
+    # Act
+    # 2,000 eth/usd price
+    # usdEntryFee is 50
+    # 2000/1 == 50/x == 0.025
+    expected_entrance_fee = Web3.toWei(0.025, "ether")
+    entrance_fee = lottery.getEntranceFee()
+    # Assert
+    assert expected_entrance_fee == entrance_fee
+
+
+def test_cant_enter_unless_started():
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    # Act/Assert
+    with pytest.raises(exceptions.VirtualMachineError):
+        lottery.enter({"from": get_account(), "value": lottery.getEntranceFee()})
+
+
+def test_can_start_and_enter_lottery():
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    account = get_account()
+    lottery.startLottery({"from": account})
+    # Act
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    # Assert
+    assert lottery.players(0) == account
+
+
+def test_can_end_lottery():
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    account = get_account()
+    lottery.startLottery({"from": account})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    fund_with_link(lottery)
+    # Act
+    lottery.endLottery({"from": account})
+    # Assert
+    assert lottery.lottery_state() == 2
+
+
+# this unit test gets close to being an integration test
+def test_can_pick_winner_correctly():
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    account = get_account()
+    lottery.startLottery({"from": account})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    lottery.enter({"from": get_account(index=1), "value": lottery.getEntranceFee()})
+    lottery.enter({"from": get_account(index=2), "value": lottery.getEntranceFee()})
+    fund_with_link(lottery)
+    # Act
+    transaction = lottery.endLottery({"from": account})
+    request_id = transaction.events["RequestedRandomness"]["requestId"]
+    STATIC_RNG = 777
+    get_contract("vrf_coordinator").callBackWithRandomness(
+        request_id, STATIC_RNG, lottery.address, {"from": account}
+    )  # this is a mock response from a chain link node that would give us a random number
+    starting_balance_of_account = account.balance()
+    balance_of_lottery = lottery.balance()
+    # Assert
+    # 777 % 3 = 0 -> divides evenly so winner is
+    assert lottery.recentWinner() == account
+    assert lottery.balance() == 0  # because we transferred all the money out
+    assert (
+        account.balance() == starting_balance_of_account + balance_of_lottery
+    )  # we should get all lottery winnings here
