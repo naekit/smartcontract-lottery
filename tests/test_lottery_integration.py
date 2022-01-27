f68940990189227597ce731514f0e458ed905309
Mykyta <mykyta.onishchuck@code.berlin>
2022-01-27 21:34:26 +0100

    finished integration test and tested and deployed on forked net and mainnet

---
+++ tests/test_lottery_integration.py
@@ -0,0 +1,31 @@
+import time
+from scripts.deploy_lottery import deploy_lottery
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account
+
+
+from brownie import network
+import pytest
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    get_account,
+    fund_with_link,
+)
+from scripts.deploy_lottery import deploy_lottery
+
+
+def test_can_pick_winner():
+    # Arrange
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    account = get_account()
+    lottery.startLottery({"from": account})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    # Act
+    fund_with_link(lottery)
+    lottery.endLottery({"from": account})
+    time.sleep(180)
+    # Assert
+    assert lottery.recentWinner() == account
+    assert lottery.balance() == 0
